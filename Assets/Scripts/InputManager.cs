db42d740eb8dc0e58e83f122b15b01258045360a
jorgevilasboas <jorge.sergipe@gmail.com>
2016-12-21 12:49:16 -0300

    Abstracting axis input

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,12 +7,34 @@ public enum Buttons
 	Left
 }
 
+public enum Condition
+{
+	GreaterThan,
+	LessThan
+}
+
 [System.Serializable]
 public class InputAxisState
 {
 	public string axisName;
 	public float offValue;
 	public Buttons button;
+	public Condition condition;
+
+	public bool value {
+		get {
+			var val = Input.GetAxis (axisName);
+
+			switch (condition) {
+			case Condition.GreaterThan:
+				return val > offValue;
+			case Condition.LessThan:
+				return val < offValue;
+			}
+			return false;
+			
+		}
+	}
 }
 
 public class InputManager : MonoBehaviour
@@ -28,6 +50,10 @@ public class InputManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		foreach (var input in inputs) {
+			if (input.value)
+				Debug.Log ("Input Detected " + input.button);
+		}
 	
 	}
 }
