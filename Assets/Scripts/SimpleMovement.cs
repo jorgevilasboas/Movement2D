50e0da4df9eedd2487017ea04f6e03ffbc2ee69c
jorgevilasboas <jorge.sergipe@gmail.com>
2016-12-21 11:38:40 -0300

    First Commit
    Addin Simple Movement script

---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour
+{
+	public float speed = 5f;
+	private Rigidbody2D body2d;
+	// Use this for initialization
+	void Start ()
+	{
+		body2d = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var val = Input.GetAxis ("Horizontal");
+		body2d.velocity = new Vector2 (speed * val, body2d.velocity.y);
+	
+	}
+}
