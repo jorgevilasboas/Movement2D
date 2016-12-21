f21826c304e68ec682499e4f4998b6eba887a674
jorgevilasboas <jorge.sergipe@gmail.com>
2016-12-21 11:51:43 -0300

    Building an input manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Buttons
+{
+	Right,
+	Left
+}
+
+[System.Serializable]
+public class InputAxisState
+{
+	public string axisName;
+	public float offValue;
+	public Buttons button;
+}
+
+public class InputManager : MonoBehaviour
+{
+	public InputAxisState[] inputs;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
